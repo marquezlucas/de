d3bda6e7776296f16479b98707c16639e8877d6d
Lucas Andrés Márquez <97174373+marquezlucas@users.noreply.github.com>
2023-10-15 20:56:20 -0300

    Add files via upload

---
+++ table_script.sql
@@ -0,0 +1,25 @@
+-- marquezlucasa_coderhouse.animals definition
+
+-- Drop table
+
+-- DROP TABLE marquezlucasa_coderhouse.animals;
+
+--DROP TABLE marquezlucasa_coderhouse.animals;
+CREATE TABLE IF NOT EXISTS marquezlucasa_coderhouse.animals
+(
+	status VARCHAR(40)   ENCODE lzo
+	,_id VARCHAR(50) NOT NULL  ENCODE lzo
+	,"user" VARCHAR(50)   ENCODE lzo
+	,text VARCHAR(100)   ENCODE lzo
+	,__v INTEGER NOT NULL  ENCODE az64
+	,source VARCHAR(30)   ENCODE lzo
+	,updatedat VARCHAR(100)   ENCODE lzo
+	,"type" VARCHAR(30)   ENCODE lzo
+	,createdat VARCHAR(100)   ENCODE lzo
+	,deleted BOOLEAN   ENCODE RAW
+	,used BOOLEAN   ENCODE RAW
+	,PRIMARY KEY (_id)
+)
+DISTSTYLE AUTO
+;
+ALTER TABLE marquezlucasa_coderhouse.animals owner to marquezlucasa_coderhouse;
