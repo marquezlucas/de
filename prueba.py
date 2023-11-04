6d7db9b08fb9e241df831d5089adeb947d5e90ef
marquezlucas <marquezlucasa@gmail.com>
2023-11-04 02:23:14 -0300

    'se crearon nuevos archivos para generar la api'

---
+++ prueba.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+import pandas as pd
+pip install
+pip install psycopg2
+import psycopg2
+
+conn = create_engine('postgresql://marquezlucasa_coderhouse:rHn8hh0y6s@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database')
