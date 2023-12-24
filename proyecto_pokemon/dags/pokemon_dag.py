90641407464c1078a8a3c429a2dc5d95ae67c2e9
Lucas Andrés Márquez <97174373+marquezlucas@users.noreply.github.com>
2023-12-24 01:27:58 -0300

    Add files via upload

--- proyecto_pokemon/dags/pokemon_dag.py
+++ proyecto_pokemon/dags/pokemon_dag.py
@@ -1,14 +1,19 @@
 import os
+# Importa las bibliotecas necesarias
+from datetime import datetime, timedelta
+from email import message
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
-# Importar funciones desde el script de conexión y carga de datos
+from airflow.operators.dummy_operator import DummyOperator  # Importa DummyOperator
+
+# Importa la función desde el otro DAG
 from scripts.conexion_carga_datos_tabla import cargar_datos_pokemon_to_redshift
+
 
 # Obtener la ruta del directorio actual
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-default_args = {
+default_args_pokemon = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
@@ -18,9 +23,9 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
+dag_pokemon = DAG(
     'pokemon_dag',
-    default_args=default_args,
+    default_args=default_args_pokemon,
     description='DAG para obtener y cargar datos de Pokémon en Redshift',
     schedule_interval=timedelta(days=1),
 )
@@ -30,6 +35,14 @@ task_obtener_datos = PythonOperator(
     task_id='obtener_datos_pokemon',
     python_callable=cargar_datos_pokemon_to_redshift,
     provide_context=True,
-    op_args=[],  # No necesitas proporcionar conn y cur aquí
-    dag=dag,
+    op_args=[],
+    dag=dag_pokemon,
 )
+# Usar DummyOperator como punto de conexión entre ambos DAGs
+dummy_operator = DummyOperator(
+    task_id='dummy_operator',
+    dag=dag_pokemon,
+)
+
+# Establecer la relación de dependencia entre las tareas
+task_obtener_datos >> dummy_operator
